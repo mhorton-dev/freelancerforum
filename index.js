24087125e997e74bde6960a5bf73a311270a2c2d
mhorton-dev <michael@mhorton.dev>
2025-05-29 08:55:12 -0500

    final changes to js and css

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+
 const getRandom = ((min, max) => {
     const minCeil = Math.ceil(min)
     const maxFloored = Math.floor(max)
 
     return Math.floor(Math.random() * (maxFloored - minCeil) + minCeil)
 })
+
 const freelancer = () => {
         const indxName = Math.floor(Math.random() * NAMES.length)
         const indxOcc = Math.floor(Math.random() * OCCUPATIONS.length)
@@ -28,12 +31,13 @@ const freelancer = () => {
             price: ranPrice
         }
         return output
-    }
+}
 
 const state = ((count) => {
     let output = []        
     for(let i = 0;i < count; i++){
         obj = freelancer()
+        output.push(obj)
     }
     return output;
 })
@@ -49,51 +53,59 @@ const prices = ((count) => {
 
 const avgPrice = (nums) => {
     let sum = nums.reduce((accumulator, current) =>
-         accumulator + current, 0)
+        accumulator + current, 0)
     return sum/nums.length;
 }
 
-const test = prices(NUM_FREELANCERS)
-
 $app = document.querySelector("#app")
-function render(){
+
+function render(arr){
     const header = document.querySelector("header")
     const headerP = document.createElement("p")
     header.appendChild(headerP)
-    headerP.innerHTML = `The average rate is \$${avgPrice(test).toFixed(2)}`
+    headerP.innerHTML = `The average rate is \$${avgPrice(prices(NUM_FREELANCERS)).toFixed(2)}`
     const table = document.createElement("table")
-    table.attribute("id", "tableFreelancers")
+    table.setAttribute("id", "tableFreelancers")
 
     //table header
-    const tbody = document.createElement("tbody")
     const thead = document.createElement("thead")
     const headerRow = document.createElement("tr")
 
-    if (test.length > 0) {
-        for (const key in test[0]){
+    console.log("array size", arr.length)
+    if (arr.length > 0) {
+        console.log("arr[0]", arr[0])
+        console.log("arr", arr)
+        const obj = arr[0]
+        for (const key in obj){
             const th = document.createElement("th")
             th.innerText = key
-            header.ariaRowCount.appendChild((th))
+            console.log(`obj key: ${key}`)
+            headerRow.appendChild(th)
         }
-        tbody.appendChild(tr)
     }
-    table.appendChild(tbody)
+    thead.appendChild(headerRow)
+    table.appendChild(thead)
 
     //Create table body
-    test.forEach((dataRow) => {
+    const tbody = document.createElement("tbody")
+
+    //populate table body
+    arr.forEach((dataRow) => {
         //create a table row
         const tr = document.createElement("tr")
         //populate each table row
         for(const key in dataRow){
             const td = document.createElement("td")
             //Populate each rows cell
-            td.innerText = dataRow(key)
+                td.innerText = dataRow[key]
             tr.appendChild(td)
         }
         tbody.appendChild(tr)
     })
+    //Append table body
+    table.appendChild(tbody)
     //Append tale to #appid
     $app.appendChild(table)
 }
 
-render();
+render(state(NUM_FREELANCERS))
